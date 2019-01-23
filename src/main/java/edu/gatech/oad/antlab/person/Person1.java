8f44205e6fc8b45137fac3aab6a4c822e76a32b2
wange011 <wange011@gmail.com>
2019-01-23 15:27:39 -0500

    edited Person1

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,7 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  String ret = input.substring(0, 2) + input.substring(2, input.length());
+	  String ret = input.substring(2, input.length()) + input.substring(0, 2);
 	  return ret;
 	}
 	

335b9e689e8364c71b3e348cc5fe0ec848cfb64b
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2019-01-25 13:44:23 -0500

    Add @param to main

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -84,6 +84,7 @@ import edu.gatech.oad.antlab.pkg2.*;
     
     /**
      * entry point for the program
+     * @param args command line arguments
      */
      public static void main(String[] args) {
         
